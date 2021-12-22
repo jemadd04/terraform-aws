e10f32ee5542db8f6cee77aeccc16433e98bc440
James Maddox <james.maddox07@gmail.com>
2021-12-22 14:04:03 -0500

    removed keys

--- var.tf
+++ var.tf
@@ -10,13 +10,6 @@ variable "location" {
   default = "eastus"
 }
 
-variable "access_key" {
-  default = "AKIATF6QK54SN6KUF2P6"
-}
-
-variable "secret_key" {
-  default = "+p+KjSeQujs3qhzA1VRw9QicfFgVKg4RVVmKB+Ow"
-}
 
 variable "ami" {
   default = "ami-04505e74c0741db8d"
